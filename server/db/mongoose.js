7b61d1e59b146050ff0067ef94b014bdd76f3243
Bobby Sun <bobby.sun@ff.com>
2019-01-04 14:32:18 -0800

    seperate test DB with development DB

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
 mongoose.promise = global.promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp', { useNewUrlParser: true });
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true });
 
 module.exports = { mongoose };
--- server/server.js
+++ server/server.js
@@ -1,3 +1,5 @@
+require('./config/config');
+
 const express = require('express');
 const bodyParser = require('body-parser');
 const { ObjectID } = require('mongodb');
@@ -109,7 +111,7 @@ app.patch('/todo/:id', (req, res) => {
 		});
 });
 
-app.listen(3000, () => {
+app.listen(process.env.PORT, () => {
 	console.log('Start listening to port 3000');
 });
 
