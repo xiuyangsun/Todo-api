65883416bf4f1d05a26e24846995c6e9b1590fc7
Bobby Sun <bobbysun@bobbys-mbp.home>
2019-01-06 13:16:41 -0800

    set up authenticate middleware

--- server/models/user.js
+++ server/models/user.js
@@ -54,6 +54,23 @@ UserSchema.methods.toJSON = function () {
 	return _.pick(userObject, ['_id', 'email']);
 };
 
+UserSchema.statics.findByToken = function (token) {
+	const User = this;
+	let decoded;
+
+	try{
+		decoded = jwt.verify(token, 'abc123');
+	} catch(e) {
+		return Promise.reject();
+	}
+
+	return User.findOne({
+		_id: decoded._id,
+		'tokens.token': token,
+		'tokens.access': 'auth'
+	});
+};
+
 const User = mongoose.model('User', UserSchema);
 
 module.exports = { User };
--- server/server.js
+++ server/server.js
@@ -8,6 +8,7 @@ const _ = require('lodash');
 const { mongoose } = require('./db/mongoose');
 const { Todo } = require('./models/todo');
 const { User } = require('./models/user');
+const { authenticate } = require('./middleware/authenticate');
 
 const app = express();
 
@@ -129,6 +130,10 @@ app.post('/users', (req, res) => {
 				.status(400)
 				.send(e);
 		});
+});
+
+app.get('/users/me', authenticate, (req,res) => {
+	res.send(req.user);
 });
 
 app.listen(process.env.PORT, () => {
